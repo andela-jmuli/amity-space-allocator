10b9f3561e1f609ce04a5d6a980c25e10cdd8ba7
andela-jmuli <joseph.muli@andela.com>
2016-09-29 17:44:39 +0300

    [Feature] Add test methods for person and room

---
+++ tests/test_person.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+from App.person import Person
+
+
+class TestPerson(unittest.TestCase):
+
+        def test_person_class_instance(self):
+                pass
+
+
+        def test_it_creates_a_person(self):
+                pass
+
+
+        def test_reallocation(self):
+                pass
+
+
+        def test_loads_people(self):
+                pass
+
+        def test_printing_unallocated(self):
+                pass
+
+
+
+
+if __name__ == '__main__':
+        unittest.main()
---
+++ tests/test_rooms.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+from App.room import Room
+
+
+class TestRoom(unittest.TestCase):
+
+        def test_room_class_instance(self):
+                pass
+
+
+        def test_it_creates_rooms(self):
+                pass
+
+
+        def test_it_prints_rooms(self):
+                pass
+
+
+        def test_it_prints_allocations(self):
+                pass
