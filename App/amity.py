a813dd7b60a705f0e3df73d73cb544b95681162d
andela-jmuli <joseph.muli@andela.com>
2016-10-03 16:46:48 +0300

    total rooms and people add

--- App/amity.py
+++ App/amity.py
@@ -2,5 +2,6 @@
 class Amity(object):
         """Amity is the SuperClass to Rooms and Person Classes"""
         def __init__(self):
-                pass
+                self.total_rooms = []
+                total_people = []
 
