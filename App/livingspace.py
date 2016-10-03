3388c8c0a00f986d3c9242ff1a9b6fbb078dcb9e
andela-jmuli <joseph.muli@andela.com>
2016-10-03 12:49:13 +0300

    [Chore] Seperate Office and livingspace classes
    Seperate classes for proper file organization

---
+++ App/livingspace.py
@@ -0,0 +1,11 @@
+from room import Room
+
+class LivingSpace(Room):
+
+        def __init__(self):
+                pass
+
+        max_capacity = 6
+
+        def allocate_living_space(self, room_name):
+                pass
---
+++ App/office.py
@@ -0,0 +1,11 @@
+from room import Room
+
+class Office(Room):
+
+        def __init__(self):
+                pass
+
+        max_capacity = 4
+
+        def allocate_office(self, room_name):
+                pass
