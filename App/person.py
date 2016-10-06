b07c69a141f563e4228bb9dfc644249371df7b8c
andela-jmuli <joseph.muli@andela.com>
2016-10-06 12:18:30 +0300

    [Feature] Add Unallocated people
    add unallocate_people method and list definition
    Modify add person method to accomodate unallocated and job type

--- App/person.py
+++ App/person.py
@@ -13,6 +13,12 @@ class Person(Amity):
             super(Amity, self).__init__()
             self.total_people = []
             self.total_rooms = []
+            self.fellows = []
+            self.staff = []
+            self.offices = []
+            self.livingspaces = []
+            self.allocated_people = []
+            self.unallocated_people = []
 
 
 
@@ -20,45 +26,73 @@ class Person(Amity):
             """
             Adds a person to the system and allocates a random room
             """
-                self.first_name = first_name
-                self.last_name = last_name
-                self.job_type = job_type
-                self.wants_accomodation = wants_accomodation
+            self.first_name = first_name
+            self.last_name = last_name
+            self.job_type = job_type
+            self.wants_accomodation = wants_accomodation
+
+            p_name = first_name + last_name
+            # add new person to the total people list
+            self.total_people.append(p_name)
 
 
-                p_name = first_name + last_name
-                # add new person to the total people list
-                self.total_people.append(p_name)
+            # check the person's job type
+            if job_type == 'Fellow':
+                self.fellows.append(p_name)
+
+                # sanity check for empty list errors
+                if len(self.offices) > 1:
+                    # allocate an office to the new fellow
+                    self.allocated_office = random.choice(self.offices)
+                else:
+                    print "There are currently no offices"
+
 
                 if wants_accomodation == 'Y':
-                    if len(self.total_rooms) > 1:
-                        # allocates a random room to the new person added
-                        allocated_room = random.choice(self.total_rooms)
-
-                        # adds new person as an occupant of the selected room
-                        allocated_room.room_occupants.append(p_name)
-                        return "Person allocated to room"
+                    # sanity check for empty list
+                    if len(self.livingspaces) > 1:
+                        # allocate a random livingspace
+                        self.allocated_livingspace = random.choice(self.livingspaces)
                     else:
-                        return "There are currently no rooms in Amity"
+                        print "There are currently no livingspaces"
+
+                elif wants_accomodation == 'N':
+                    self.unallocated_people.append(p_name)
+
+
+            elif job_type == 'Staff':
+                self.staff.append(p_name)
+                self.allocated_office = random.choice(self.offices)
+
+                if wants_accomodation == 'Y':
+                    return "Staff members are not allocated livingspaces"
+
+
+        def print_unallocated(self, filename, *args):
+            """
+            prints out unallocated people to a specified file
+            """
+
+            # if file output option specified in arguments, write to file
+            if '-o' in args:
+                with open(unallocated, 'wb') as f:
+                    pickle.dump(self.unallocated_people, f)
+            # if file output option not specified, write to screen(console)
+            else:
+                for person in self.unallocated_people:
+                    print person
 
 
         def reallocate_person(self, person_id, room_name):
             """
             Reallocates a person to a new room
             """
-                pass
+            pass
 
 
         def load_people(self, filename):
             """
             Adds people to rooms from a text file
             """
-                pass
-
-
-        def print_unallocated(self, filename):
-            """
-            prints out unallocated people to a specified file
-            """
-                pass
+            pass
 
